56062a4652cbc174fd88c49f564c5a91f93d71c5
Evgeni Dmitriev <thybzi@gmail.com>
2024-03-25 09:02:04 +0300

    Start porting cars project

---
+++ src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.css';
+
+export function Button({
+    text,
+    auxClass = '',
+}) {
+    return (
+        <div
+            className={'Button ' + auxClass}
+        >
+            {text}
+        </div>
+    );
+}
---
+++ src/components/CatalogContent/CatalogContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CatalogContent.css';
+
+export function CatalogContent({
+    galleryItems = [],
+}) {
+    return (
+        <div className="CatalogContent">
+            {galleryItems.map((itemData, i) => (
+                <div key={i}>
+                    {itemData.title}
+                </div>
+            ))}
+        </div>
+    );
+}
