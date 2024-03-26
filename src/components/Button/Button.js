a854446c47c7ac54c6177f3dc8a4e42056e99cbc
Evgeni Dmitriev <thybzi@gmail.com>
2024-03-26 07:41:04 +0300

    Use clsx for class names

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 import './Button.css';
 
 export function Button({
@@ -7,7 +8,7 @@ export function Button({
 }) {
     return (
         <div
-            className={'Button ' + auxClass}
+            className={clsx(['Button', auxClass])}
         >
             {text}
         </div>
