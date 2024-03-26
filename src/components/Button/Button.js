ebd6a9107080ad486cc8b420d23117280164316a
Evgeni Dmitriev <edmitriev@ivi.ru>
2024-03-27 01:45:48 +0300

    Limit cars, showing more by button click

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,10 +4,12 @@ import './Button.css';
 export function Button({
     text,
     auxClass = '',
+    ...props
 }) {
     return (
         <div
             className={clsx(['Button', auxClass])}
+            {...props}
         >
             {text}
         </div>
--- src/components/CatalogMore/CatalogMore.js
+++ src/components/CatalogMore/CatalogMore.js
@@ -1,12 +1,15 @@
 import {Button} from '../Button/Button';
 import './CatalogMore.css';
 
-export function CatalogMore() {
+export function CatalogMore({
+    buttonClickHandler,
+}) {
     return (
         <div className="CatalogMore">
             <Button
                 auxClass='CatalogMore__button'
                 text='Show more cars'
+                onClick={buttonClickHandler}
             />
             <div className="CatalogMore__caption">120 Cars</div>
         </div>
