49f120e66db349c02d1a93875a687453b5eca924
Evgeni Dmitriev <edmitriev@ivi.ru>
2024-03-27 19:15:27 +0300

    Stylize gray container as MainContent

--- src/pages/CatalogPage/CatalogPage.js
+++ src/pages/CatalogPage/CatalogPage.js
@@ -1,10 +1,13 @@
 import {useLoaderData} from 'react-router-dom';
+import {MainContent} from '../../components/MainContent/MainContent';
 import {CatalogContent} from '../../components/CatalogContent/CatalogContent';
 
 export function CatalogPage() {
     const galleryItems = useLoaderData();
 
     return (
-        <CatalogContent galleryItems={galleryItems}/>
+        <MainContent>
+            <CatalogContent galleryItems={galleryItems}/>
+        </MainContent>
     );
 }
