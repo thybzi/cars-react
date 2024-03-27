526239f1603b37b5c931d2f6495969089f09185d
Evgeni Dmitriev <thybzi@gmail.com>
2024-03-27 09:03:41 +0300

    Add pages and routing

---
+++ src/pages/App/App.js
@@ -0,0 +1,28 @@
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import {HomePage} from '../HomePage/HomePage';
+import {CatalogPage} from '../CatalogPage/CatalogPage';
+import './App.scss';
+
+export function App() {
+    const apiUrl = 'https://660247539d7276a75552f2f5.mockapi.io/cars/list';
+
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <HomePage/>,
+        },
+        {
+            path: '/catalog',
+            element: <CatalogPage/>,
+            loader: async () => {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                return data;
+            },
+        },
+    ]);
+
+    return (
+        <RouterProvider router={router}/>
+    );
+}
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,7 @@
+import {Link} from 'react-router-dom';
+
+export function HomePage() {
+    return (
+        <Link to="/catalog">Go to catalog</Link>
+    );
+}
