fef53958ebbd0c6dfdeb738a0e746330e227f5c8
Evgeni Dmitriev <edmitriev@ivi.ru>
2024-03-28 04:47:45 +0300

    Revert "Hide favorite icons on favorite page"
    This reverts commit 9370dc4e18a0fc027467aeeed2699db142844e35.

--- src/app/App.js
+++ src/app/App.js
@@ -51,7 +51,7 @@ export function App() {
         },
         {
             path: '/favorites',
-            element: <CatalogPage noGalleryItemsFavoriteIcon={true}/>,
+            element: <CatalogPage/>,
             loader: async () => {
                 const res = await fetch(API_URL);
                 const data = await res.json();
--- src/pages/CatalogPage.js
+++ src/pages/CatalogPage.js
@@ -3,19 +3,14 @@ import {Menu} from '../components/Menu/Menu';
 import {MainContent} from '../components/MainContent/MainContent';
 import {CatalogContent} from '../components/CatalogContent/CatalogContent';
 
-export function CatalogPage({
-    noGalleryItemsFavoriteIcon = false,
-}) {
+export function CatalogPage() {
     const galleryItems = useLoaderData();
 
     return (
         <>
             <Menu/>
             <MainContent>
-                <CatalogContent
-                    galleryItems={galleryItems}
-                    noGalleryItemsFavoriteIcon={noGalleryItemsFavoriteIcon}
-                />
+                <CatalogContent galleryItems={galleryItems}/>
             </MainContent>
         </>
     );
