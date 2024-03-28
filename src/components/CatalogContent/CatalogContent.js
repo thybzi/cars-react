f930a301c6f065f8b632f0850e8160c34852e802
Evgeni Dmitriev <thybzi@gmail.com>
2024-03-28 08:13:28 +0300

    Add content wrapper component

--- src/components/CatalogContent/CatalogContent.js
+++ src/components/CatalogContent/CatalogContent.js
@@ -1,4 +1,3 @@
-import './CatalogContent.scss';
 import {useState} from 'react';
 import {CatalogItemsGallery} from '../CatalogItemsGallery/CatalogItemsGallery';
 import {CatalogMore} from '../CatalogMore/CatalogMore';
@@ -19,7 +18,7 @@ export function CatalogContent({
     }
 
     return (
-        <div className="CatalogContent">
+        <>
             <CatalogItemsGallery
                 items={galleryItems.slice(0, shownItemsCount)}
             />
@@ -29,6 +28,6 @@ export function CatalogContent({
                     galleryItemsCount={galleryItems.length}
                 />
             )}
-        </div>
+        </>
     );
 }
