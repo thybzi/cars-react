f7fec2403cd7942f3f914b8042394e499f7e6afd
Evgeni Dmitriev <thybzi@gmail.com>
2024-03-29 08:11:35 +0300

    Show favorite status on item page

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -1,13 +1,17 @@
 import {useLoaderData} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 import {Menu} from '../components/Menu/Menu';
 import {MainContent} from '../components/MainContent/MainContent';
 
 export function ItemPage() {
     const {
+        id,
         image,
         title,
         description,
     } = useLoaderData();
+
+    const isFavorite = useSelector((state) => (state.favorites.includes(id)));
 
     return (
         <>
@@ -17,7 +21,7 @@ export function ItemPage() {
                     src={image}
                     alt={`${title} photo`}
                 />
-                <h1>{title}</h1>
+                <h1>{title} {isFavorite && <small>(favorite!)</small>}</h1>
                 <div>{description}</div>
             </MainContent>
         </>
