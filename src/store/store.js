c9c13b8af27c532f51051e8b07339188afdadbd6
Evgeni Dmitriev <thybzi@gmail.com>
2024-04-01 07:42:20 +0300

    Use RTK for configuring store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import {createStore} from 'redux';
+import {configureStore} from '@reduxjs/toolkit';
 import {reducer} from './reducer';
 
 const lsKey = 'cars';
@@ -8,7 +8,10 @@ const initialState = lsValue ? JSON.parse(lsValue) : {
     favorites: [],
 };
 
-export const store = createStore(reducer, initialState);
+export const store = configureStore({
+    reducer,
+    preloadedState: initialState,
+});
 
 store.subscribe(() => {
     localStorage.setItem(lsKey, JSON.stringify(store.getState()));
