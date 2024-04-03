60234109da1a7e2516e5601147db030b76af5000
Evgeni Dmitriev <edmitriev@ivi.ru>
2024-04-03 20:18:41 +0300

    Display loading state

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,13 +1,17 @@
-import {Outlet} from 'react-router-dom';
+import {Outlet, useNavigation} from 'react-router-dom';
+import {Loading} from '../components/Loading/Loading';
 import {Menu} from '../components/Menu/Menu';
 import {MainContent} from '../components/MainContent/MainContent';
 
 export function Layout() {
+    const navigation = useNavigation();
+    const isLoading = (navigation.state === 'loading');
+
     return (
         <>
             <Menu/>
             <MainContent>
-                <Outlet/>
+                {isLoading ? <Loading/> : <Outlet/>}
             </MainContent>
         </>
     );
