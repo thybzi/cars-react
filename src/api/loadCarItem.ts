ad1c70a91c8089a66e899e8ea6fb320811e6a241
Evgeni Dmitriev <edmitriev@ivi.ru>
2024-04-04 01:30:54 +0300

    Add some error handling

--- src/api/loadCarItem.ts
+++ src/api/loadCarItem.ts
@@ -4,6 +4,11 @@ import {carsApiUrl} from './values';
 
 export const loadCarItem = async (itemId: string) => {
     const res = await fetch(`${carsApiUrl}/${itemId}`);
+
+    if (!res.ok) {
+        throw new Error(`${res.status} ${res.statusText}`);
+    }
+
     const data = await res.json() as CarItemData;
     return processItemData(data);
 };
--- src/api/loadCarsList.ts
+++ src/api/loadCarsList.ts
@@ -4,6 +4,11 @@ import type {CarItemData} from './types';
 
 export const loadCarsList = async () => {
     const res = await fetch(carsApiUrl);
+
+    if (!res.ok) {
+        throw new Error(`${res.status} ${res.statusText}`);
+    }
+
     const data = await res.json() as CarItemData[];
     return data.map(processItemData);
 };
