08d8cd7a1f418da08ed90afe6ac54759616e09a8
Evgeni Dmitriev <edmitriev@ivi.ru>
2024-04-04 22:51:37 +0300

    Adjustments for DOM tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,13 @@
 import type {Config} from '@jest/types';
 
 const config: Config.InitialOptions = {
+    testEnvironment: 'jsdom',
     verbose: true,
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
+    },
+    moduleNameMapper: {
+        '^.+\\.module.(scss)$': '<rootDir>/mocks/styleModule.js',
     },
 };
 
